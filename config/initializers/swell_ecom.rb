01d9e7f908b6ada36cd9581220ec66527109918e
Gk <gk@gkparishphilp.com>
2017-02-08 08:16:48 -0800

    - ecom & prod

---
+++ config/initializers/swell_ecom.rb
@@ -0,0 +1,12 @@
+SwellEcom.configure do |config|
+
+	config.origin_address = {
+		street: '2120 Jimmy Durante Blvd Ste. 108',
+		city: 'Del Mar',
+		state: 'CA',
+		zip: '92014'
+	}
+
+	config.order_email_from = 'no-reply@amraplife.com'
+
+end
