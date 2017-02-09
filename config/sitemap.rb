6bf960b18f03f400e2587be020677a921f52fef7
Gk <gk@gkparishphilp.com>
2017-02-09 10:24:54 -0800

    - sitemap bug

--- config/sitemap.rb
+++ config/sitemap.rb
@@ -14,23 +14,26 @@ SitemapGenerator::Sitemap.sitemaps_path = 'sitemaps/'
 
 SitemapGenerator::Sitemap.create do
 	add '/'
+	add '/about'
+	add '/privacy'
+	add '/terms'
 	add '/blog'
-	add '/recipes'
-	add '/places'
-	add '/workouts'
+	#add '/recipes'
+	#add '/places'
+	#add '/workouts'
 	SwellMedia::Media.active.each do |media|
 		add media.path, lastmod: media.updated_at
 	end
-	Eqipment.active.each do |equipment|
-		add equipment.path, lastmod: equipment.updated_at
-	end
-	Recipe.active.each do |recipe|
-		add recipe.path, lastmod: recipe.updated_at
-	end
-	Movement.active.each do |movement|
-		add movement.path, lastmod: movement.updated_at
-	end
-	Workout.active.each do |workout|
-		add workout.path, lastmod: workout.updated_at
-	end
+	# Equipment.active.each do |equipment|
+	# 	add equipment.path, lastmod: equipment.updated_at
+	# end
+	# Recipe.active.each do |recipe|
+	# 	add recipe.path, lastmod: recipe.updated_at
+	# end
+	# Movement.active.each do |movement|
+	# 	add movement.path, lastmod: movement.updated_at
+	# end
+	# Workout.active.each do |workout|
+	# 	add workout.path, lastmod: workout.updated_at
+	# end
 end
