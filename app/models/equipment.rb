89170d7f055ba1894e654dc3bdc258f912ac5e94
Gk <gk@gkparishphilp.com>
2017-02-09 10:42:47 -0800

    - equipment bug

--- app/models/equipment.rb
+++ app/models/equipment.rb
@@ -2,12 +2,28 @@
 class Equipment < ActiveRecord::Base 
 
 	validates		:title, presence: true, unless: :allow_blank_title?
+
+	attr_accessor	:slug_pref
 	
 	acts_as_nested_set
 	
 	has_many :movements
 	has_many :workout_movements
 	has_many :workouts, -> { distinct }, through: :workout_movements
+
+	include FriendlyId
+	friendly_id :slugger, use: [ :slugged, :history ]
+	
+
+
+	def slugger
+		if self.slug_pref.present?
+			self.slug = nil # friendly_id 5.0 only updates slug if slug field is nil
+			return self.slug_pref
+		else
+			return self.title
+		end
+	end
 	
 
 	def to_s
