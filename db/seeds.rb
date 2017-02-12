e6d678f5f8bf4d7745a0ed9105accf455ed6444e
Gk <gk@gkparishphilp.com>
2017-02-12 09:17:57 -0800

    - seed some users

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,2 +1,4 @@
 puts "Adding Gk"
-u = User.create name: 'Gk', email: 'gk@gk.com', role: :admin, password: '1234'
+u = User.create name: 'Gk', email: 'gk@amraplife.com', role: :admin, password: '1234'
+u = User.create name: 'Michael', email: 'michael@amraplife.com', role: :admin, password: '1234'
+u = User.create name: 'Tina', email: 'tina@amraplife.com', role: :admin, password: '1234'
