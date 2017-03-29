50494e1dbad1bf0ef2e20887cf76c26fe0023d4b
Michael <meister@spacekace.com>
2017-03-29 16:11:40 -0700

    starting on places

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -1,11 +1,23 @@
 
-class PlacesController < ApplicationController 
+class PlacesController < ApplicationController
+	before_action :set_place, only: :show
+
 
 	def index
-		
+		@places = Places.published.order( :title )
+		@places = @places.page( params[:page] )
+		set_page_meta( title: 'Places )°( AMRAP Life' )
 	end
 
 	def show
-		
+		set_page_meta( title: @place.title, description: (@place.description || @place.content).html_safe )
 	end
+
+
+  private
+	# Use callbacks to share common setup or constraints between actions.
+	def set_place
+		@place = Place.published.friendly.find(params[:id])
+	end
+
 end
