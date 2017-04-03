1d7cb9b030a4adf825d96e60988d05e6b01c1721
Michael <meister@spacekace.com>
2017-04-03 08:06:09 -0700

    learn more contact form

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -18,7 +18,7 @@ class PlacesController < ApplicationController
 	end
 
 
-  private
+	private
 	# Use callbacks to share common setup or constraints between actions.
 	def set_place
 		@media = @place = Place.published.friendly.find(params[:id])
