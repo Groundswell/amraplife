d0a438e953ee5aac616bb75e02e566ca177f93c6
Gk <gk@gkparishphilp.com>
2017-05-26 06:21:31 -0700

    - new theme

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,7 @@
 # Version of your assets, change this if you want to expire all your assets.
 Rails.application.config.assets.version = '1.0'
 
-Rails.application.config.assets.precompile += %w( app_theme.css cube.css cube.js )
+Rails.application.config.assets.precompile += %w( app_theme.css app_theme.js cube.css cube.js )
 
 # Add additional assets to the asset load path
 # Rails.application.config.assets.paths << Emoji.images_path
