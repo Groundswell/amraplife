02a061a7c18eeaafa18003d2ebfa115cde67efef
Gk <gk@gkparishphilp.com>
2017-05-27 17:46:52 -0700

    - scrollspy won't work

--- config/routes.rb
+++ config/routes.rb
@@ -35,7 +35,7 @@ Rails.application.routes.draw do
 		get :preview, on: :member
 	end
 
-	resources :terms, only: :index, path: 'terms'
+	resources :terms, only: :index, path: 'xfit-terms'
 	resources :term_admin do
 		delete :empty_trash, on: :collection
 	end
