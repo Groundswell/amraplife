6e2d70d76b56907870b6005fcef7c884c722ba63
Gk <gk@gkparishphilp.com>
2017-06-02 10:53:00 -0700

    - glossary working

--- app/controllers/terms_controller.rb
+++ app/controllers/terms_controller.rb
@@ -3,7 +3,9 @@ class TermsController < ApplicationController
 	def index
 		@terms = Term.published
 		@terms = @terms.sort_by{ |t| t.title.sub(/^the /i, '' ).sub(/^a /i, '' ).downcase }
-		
+		@letters = Term.pluck( "left( title, 1 )" ).sort.uniq
+		#@letters = ('a'..'z').to_a
+		@anchor_idx = 0
 		# if params[:q].present?
 		# 	match = params[:q].downcase.singularize.gsub( /\s+/, '' )
 		# 	@terms = @terms.where( "lower(REGEXP_REPLACE(title, '\s', '' )) = :m", m: match )
