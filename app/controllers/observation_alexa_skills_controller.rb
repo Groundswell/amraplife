9235a7975e2de9e02b6b9a66fac2ad2d1a8c44dd
Michael <meister@spacekace.com>
2017-06-16 14:48:40 -0700

    stubbing out intent responses

--- app/controllers/observation_alexa_skills_controller.rb
+++ app/controllers/observation_alexa_skills_controller.rb
@@ -4,39 +4,43 @@ class ObservationAlexaSkillsController < ActionController::Base
 
 	def create
 		# @todo Check that it's a valid Alexa request
-
-		request = AlexaRubykit.build_request( JSON.parse(request.raw_post) )
-		session = request.session
-		response = AlexaRubykit::Response.new
+		alexa_request = AlexaRubykit.build_request( JSON.parse(request.raw_post) )
+		alexa_session = request.session
+		alexa_response = AlexaRubykit::Response.new
 
 		# Response
-		if (request.type == 'LAUNCH_REQUEST')
+		if (alexa_request.type == 'LAUNCH_REQUEST')
 			# Process your Launch Request
-			response.add_speech('Welcome ')
-			# response.add_hash_card( { :title => 'Ruby Run', :subtitle => 'Ruby Running Ready!' } )
+			alexa_response.add_speech('Welcome ')
+			# alexa_response.add_hash_card( { :title => 'Ruby Run', :subtitle => 'Ruby Running Ready!' } )
 		end
 
-		if (request.type == 'INTENT_REQUEST')
+		if (alexa_request.type == 'INTENT_REQUEST')
 			# Process your Intent Request
-			# puts "#{request.slots}"
-			# puts "#{request.name}"
+			puts "#{alexa_request.slots}"
+			puts "#{alexa_request.name}"
 
-			if request.name == 'LogMetricIntent'
-				response.add_speech("Logging Metric")
-				response.add_hash_card( { :title => 'Ruby Intent', :subtitle => "Intent #{request.name}" } )
+			if alexa_request.name == 'LogMetricObservationIntent'
+				alexa_response.add_speech("Logging Metric")
+				# alexa_response.add_hash_card( { :title => 'Logging Metric', :subtitle => "Intent #{alexa_request.name}" } )
+			elsif alexa_request.name == 'LogStartObservationIntent'
+				alexa_response.add_speech("Starting timer")
+				# alexa_response.add_hash_card( { :title => 'Logging Metric', :subtitle => "Intent #{alexa_request.name}" } )
+			elsif alexa_request.name == 'LogStopObservationIntent'
+				alexa_response.add_speech("Stopping timer")
+				# alexa_response.add_hash_card( { :title => 'Logging Metric', :subtitle => "Intent #{alexa_request.name}" } )
 			else
-				response.add_speech("Recieved Intent #{request.name}")
+				alexa_response.add_speech("Recieved Intent #{alexa_request.name}")
 			end
 		end
 
-		if (request.type =='SESSION_ENDED_REQUEST')
+		if (alexa_request.type =='SESSION_ENDED_REQUEST')
 			# Wrap up whatever we need to do.
-			puts "#{request.type}"
-			puts "#{request.reason}"
+			puts "#{alexa_request.type}"
+			puts "#{alexa_request.reason}"
 			halt 200
 		end
 
-		# Return response
-		render json: response.build_response
+		render json: alexa_response.build_response
 	end
 end
