eb6d0833b2956642be588f4a7a43b80026303f67
Michael <meister@spacekace.com>
2017-06-30 21:38:24 -0700

    bug fix

--- app/controllers/observation_google_actions_controller.rb
+++ app/controllers/observation_google_actions_controller.rb
@@ -19,7 +19,7 @@ class ObservationGoogleActionsController < ActionController::Base
 				@bot_service = ObservationBotService.new( response: action_response, user: nil, params: {}, dialog: DEFAULT_DIALOG )
 
 				request_text = params[:inputs].first[:raw_inputs].first[:query]
-				request_text = request_text.gsub(/^.* LifeMeter/, '').strip
+				request_text = request_text.gsub(/^.* (LifeMeter|life meter)/i, '').strip
 
 				unless @bot_service.respond_to_text( request_text )
 					action_response.add_speech( "Sorry, I don't know about that." )
