2d540b1612906158a0a1ac5e232a04b6cf9735e8
Michael <meister@spacekace.com>
2017-08-29 09:53:14 -0700

    bug fix

--- app/models/user.rb
+++ app/models/user.rb
@@ -27,7 +27,7 @@ class User < SwellMedia::User
 
 		user = oauth_credential.user
 
-		user ||= self.where( email: auth.info.email ).first_or_create do |user|
+		user ||= where( email: auth.info.email ).first_or_create do |user|
 
 			user.email = auth.info.email
 			user.password = Devise.friendly_token[0,20]
