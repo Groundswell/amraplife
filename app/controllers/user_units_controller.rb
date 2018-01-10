599761dd0423ab194d39bacff8ea0ef6c433a2be
Gk <gk@gkparishphilp.com>
2018-01-10 09:28:36 -0800

    - user_units?

--- app/controllers/user_units_controller.rb
+++ app/controllers/user_units_controller.rb
@@ -1,12 +1,46 @@
 class UserUnitsController < ApplicationController
 
 	before_filter :authenticate_user!
+	before_filter :get_metric, except: [ :create, :index ]
 
 	layout 'lifemeter'
 
 
-	def index
-		@units = current_user.units.page
+	def create
+		@unit = current_user.units.new( unit_params )
+		
+		if @unit.save
+			set_flash "Unit Saved"
+		else
+			set_flash "Unit could not be saved", :danger, @unit
+		end
+		redirect_to edit_user_units_path( @unit )
 	end
+
+	def destroy
+		@unit.destroy
+		set_flash "unit Deleted"
+		redirect_to user_units_path
+	end
+
+	def index
+		@units = current_user.units
+	end
+
+	def update
+		@unit.update( unit_params )
+		redirect_to :back
+	end
+
+
+	private
+
+		def get_unit
+			@unit = current_user.units.friendly.find( params[:id] )
+		end
+
+		def unit_params
+			params.require( :unit ).permit( :name, :status, :abbrev, :aliases_csv, :metric_id, :conversion_factor, :unit_type )
+		end
 
 end
